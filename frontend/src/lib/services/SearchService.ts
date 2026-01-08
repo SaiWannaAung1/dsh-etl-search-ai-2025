7e249d6170a51ec4baa8019f4b3c73881c1d821f
SaiWannaAung1 <saiwannaaung095@gmail.com>
2026-01-08 07:35:50 +0000

    Add search frontend [skip ci]

---
+++ frontend/src/lib/services/SearchService.ts
@@ -0,0 +1,12 @@
+﻿import { BaseService } from './BaseService';
+import type { Dataset } from '$lib/models/Dataset';
+
+export class SearchService extends BaseService {
+    async searchDatasets(query: string): Promise<Dataset[]> {
+        // Just call the inherited 'post' method
+        // Endpoint starts with / because it appends to BASE_URL
+        return await this.post<Dataset[]>('/Search', { query });
+    }
+}
+
+export const searchService = new SearchService();
